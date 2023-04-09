9b5bb7c0454a667bbcd7eaad94a0803feb1593a8
unknown <steve1998kewen@gmail.com>
2023-04-09 12:58:07 -0700

    fix windows compatibility issue

--- GroundingDINO/groundingdino/util/slconfig.py
+++ GroundingDINO/groundingdino/util/slconfig.py
@@ -8,6 +8,7 @@ import sys
 import tempfile
 from argparse import Action
 from importlib import import_module
+import platform
 
 from addict import Dict
 from yapf.yapflib.yapf_api import FormatCode
@@ -80,6 +81,8 @@ class SLConfig(object):
             with tempfile.TemporaryDirectory() as temp_config_dir:
                 temp_config_file = tempfile.NamedTemporaryFile(dir=temp_config_dir, suffix=".py")
                 temp_config_name = osp.basename(temp_config_file.name)
+                if platform.system() == 'Windows':
+                    temp_config_file.close()
                 shutil.copyfile(filename, osp.join(temp_config_dir, temp_config_name))
                 temp_module_name = osp.splitext(temp_config_name)[0]
                 sys.path.insert(0, temp_config_dir)
