078e4dd0ed276717cf8511173c7e90f0f192cdd2
Julienlin <julien.lin@osedea.com>
2023-04-14 03:07:50 -0400

    test releasing memory

--- automatic_label_demo.py
+++ automatic_label_demo.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import copy
+import gc
 
 import numpy as np
 import json
@@ -13,10 +14,13 @@ import GroundingDINO.groundingdino.datasets.transforms as T
 from GroundingDINO.groundingdino.models import build_model
 from GroundingDINO.groundingdino.util import box_ops
 from GroundingDINO.groundingdino.util.slconfig import SLConfig
-from GroundingDINO.groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
+from GroundingDINO.groundingdino.util.utils import (
+    clean_state_dict,
+    get_phrases_from_posmap,
+)
 
 # segment anything
-from segment_anything import build_sam, SamPredictor 
+from segment_anything import build_sam, SamPredictor
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -54,43 +58,47 @@ def generate_caption(raw_image, device):
     return caption
 
 
-def generate_tags(caption, split=',', max_tokens=100, model="gpt-3.5-turbo"):
+def generate_tags(caption, split=",", max_tokens=100, model="gpt-3.5-turbo"):
     prompt = [
         {
-            'role': 'system',
-            'content': 'Extract the unique nouns in the caption. Remove all the adjectives. ' + \
-                       f'List the nouns in singular form. Split them by "{split} ". ' + \
-                       f'Caption: {caption}.'
+            "role": "system",
+            "content": "Extract the unique nouns in the caption. Remove all the adjectives. "
+            + f'List the nouns in singular form. Split them by "{split} ". '
+            + f"Caption: {caption}.",
         }
     ]
-    response = openai.ChatCompletion.create(model=model, messages=prompt, temperature=0.6, max_tokens=max_tokens)
-    reply = response['choices'][0]['message']['content']
+    response = openai.ChatCompletion.create(
+        model=model, messages=prompt, temperature=0.6, max_tokens=max_tokens
+    )
+    reply = response["choices"][0]["message"]["content"]
     # sometimes return with "noun: xxx, xxx, xxx"
-    tags = reply.split(':')[-1].strip()
+    tags = reply.split(":")[-1].strip()
     return tags
 
 
 def check_caption(caption, pred_phrases, max_tokens=100, model="gpt-3.5-turbo"):
-    object_list = [obj.split('(')[0] for obj in pred_phrases]
+    object_list = [obj.split("(")[0] for obj in pred_phrases]
     object_num = []
     for obj in set(object_list):
-        object_num.append(f'{object_list.count(obj)} {obj}')
-    object_num = ', '.join(object_num)
+        object_num.append(f"{object_list.count(obj)} {obj}")
+    object_num = ", ".join(object_num)
     print(f"Correct object number: {object_num}")
 
     prompt = [
         {
-            'role': 'system',
-            'content': 'Revise the number in the caption if it is wrong. ' + \
-                       f'Caption: {caption}. ' + \
-                       f'True object number: {object_num}. ' + \
-                       'Only give the revised caption: '
+            "role": "system",
+            "content": "Revise the number in the caption if it is wrong. "
+            + f"Caption: {caption}. "
+            + f"True object number: {object_num}. "
+            + "Only give the revised caption: ",
         }
     ]
-    response = openai.ChatCompletion.create(model=model, messages=prompt, temperature=0.6, max_tokens=max_tokens)
-    reply = response['choices'][0]['message']['content']
+    response = openai.ChatCompletion.create(
+        model=model, messages=prompt, temperature=0.6, max_tokens=max_tokens
+    )
+    reply = response["choices"][0]["message"]["content"]
     # sometimes return with "Caption: xxx, xxx, xxx"
-    caption = reply.split(':')[-1].strip()
+    caption = reply.split(":")[-1].strip()
     return caption
 
 
@@ -99,13 +107,17 @@ def load_model(model_config_path, model_checkpoint_path, device):
     args.device = device
     model = build_model(args)
     checkpoint = torch.load(model_checkpoint_path, map_location="cpu")
-    load_res = model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
+    load_res = model.load_state_dict(
+        clean_state_dict(checkpoint["model"]), strict=False
+    )
     print(load_res)
     _ = model.eval()
     return model
 
 
-def get_grounding_output(model, image, caption, box_threshold, text_threshold,device="cpu"):
+def get_grounding_output(
+    model, image, caption, box_threshold, text_threshold, device="cpu"
+):
     caption = caption.lower()
     caption = caption.strip()
     if not caption.endswith("."):
@@ -133,7 +145,9 @@ def get_grounding_output(model, image, caption, box_threshold, text_threshold,de
     pred_phrases = []
     scores = []
     for logit, box in zip(logits_filt, boxes_filt):
-        pred_phrase = get_phrases_from_posmap(logit > text_threshold, tokenized, tokenlizer)
+        pred_phrase = get_phrases_from_posmap(
+            logit > text_threshold, tokenized, tokenlizer
+        )
         pred_phrases.append(pred_phrase + f"({str(logit.max().item())[:4]})")
         scores.append(logit.max().item())
 
@@ -144,7 +158,7 @@ def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
     else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
@@ -153,7 +167,9 @@ def show_mask(mask, ax, random_color=False):
 def show_box(box, ax, label):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2)) 
+    ax.add_patch(
+        plt.Rectangle((x0, y0), w, h, edgecolor="green", facecolor=(0, 0, 0, 0), lw=2)
+    )
     ax.text(x0, y0, label)
 
 
@@ -165,32 +181,32 @@ def save_mask_data(output_dir, caption, mask_list, box_list, label_list):
         mask_img[mask.cpu().numpy()[0] == True] = value + idx + 1
     plt.figure(figsize=(10, 10))
     plt.imshow(mask_img.numpy())
-    plt.axis('off')
-    plt.savefig(os.path.join(output_dir, 'mask.jpg'), bbox_inches="tight", dpi=300, pad_inches=0.0)
+    plt.axis("off")
+    plt.savefig(
+        os.path.join(output_dir, "mask.jpg"),
+        bbox_inches="tight",
+        dpi=300,
+        pad_inches=0.0,
+    )
 
-    json_data = {
-        'caption': caption,
-        'mask':[{
-            'value': value,
-            'label': 'background'
-        }]
-    }
+    json_data = {"caption": caption, "mask": [{"value": value, "label": "background"}]}
     for label, box in zip(label_list, box_list):
         value += 1
-        name, logit = label.split('(')
-        logit = logit[:-1] # the last is ')'
-        json_data['mask'].append({
-            'value': value,
-            'label': name,
-            'logit': float(logit),
-            'box': box.numpy().tolist(),
-        })
-    with open(os.path.join(output_dir, 'label.json'), 'w') as f:
+        name, logit = label.split("(")
+        logit = logit[:-1]  # the last is ')'
+        json_data["mask"].append(
+            {
+                "value": value,
+                "label": name,
+                "logit": float(logit),
+                "box": box.numpy().tolist(),
+            }
+        )
+    with open(os.path.join(output_dir, "label.json"), "w") as f:
         json.dump(json_data, f)
-    
+
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser("Grounded-Segment-Anything Demo", add_help=True)
     parser.add_argument("--config", type=str, required=True, help="path to config file")
     parser.add_argument(
@@ -199,19 +215,36 @@ if __name__ == "__main__":
     parser.add_argument(
         "--sam_checkpoint", type=str, required=True, help="path to checkpoint file"
     )
-    parser.add_argument("--input_image", type=str, required=True, help="path to image file")
+    parser.add_argument(
+        "--input_image", type=str, required=True, help="path to image file"
+    )
     parser.add_argument("--split", default=",", type=str, help="split for text prompt")
     parser.add_argument("--openai_key", type=str, required=True, help="key for chatgpt")
-    parser.add_argument("--openai_proxy", default=None, type=str, help="proxy for chatgpt")
     parser.add_argument(
-        "--output_dir", "-o", type=str, default="outputs", required=True, help="output directory"
+        "--openai_proxy", default=None, type=str, help="proxy for chatgpt"
+    )
+    parser.add_argument(
+        "--output_dir",
+        "-o",
+        type=str,
+        default="outputs",
+        required=True,
+        help="output directory",
     )
 
-    parser.add_argument("--box_threshold", type=float, default=0.25, help="box threshold")
-    parser.add_argument("--text_threshold", type=float, default=0.2, help="text threshold")
-    parser.add_argument("--iou_threshold", type=float, default=0.5, help="iou threshold")
+    parser.add_argument(
+        "--box_threshold", type=float, default=0.25, help="box threshold"
+    )
+    parser.add_argument(
+        "--text_threshold", type=float, default=0.2, help="text threshold"
+    )
+    parser.add_argument(
+        "--iou_threshold", type=float, default=0.5, help="iou threshold"
+    )
 
-    parser.add_argument("--device", type=str, default="cpu", help="running on cpu only!, default=False")
+    parser.add_argument(
+        "--device", type=str, default="cpu", help="running on cpu only!, default=False"
+    )
     args = parser.parse_args()
 
     # cfg
@@ -236,8 +269,6 @@ if __name__ == "__main__":
     os.makedirs(output_dir, exist_ok=True)
     # load image
     image_pil, image = load_image(image_path)
-    # load model
-    model = load_model(config_file, grounded_checkpoint, device=device)
 
     # visualize raw image
     image_pil.save(os.path.join(output_dir, "raw_image.jpg"))
@@ -248,9 +279,13 @@ if __name__ == "__main__":
     # but there are some bugs...
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
     if device == "cuda":
-        blip_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", torch_dtype=torch.float16).to("cuda")
+        blip_model = BlipForConditionalGeneration.from_pretrained(
+            "Salesforce/blip-image-captioning-large", torch_dtype=torch.float16
+        ).to("cuda")
     else:
-        blip_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+        blip_model = BlipForConditionalGeneration.from_pretrained(
+            "Salesforce/blip-image-captioning-large"
+        )
     caption = generate_caption(image_pil, device=device)
     # Currently ", " is better for detecting single tags
     # while ". " is a little worse in some case
@@ -258,6 +293,13 @@ if __name__ == "__main__":
     print(f"Caption: {caption}")
     print(f"Tags: {text_prompt}")
 
+    blip_model.cpu()
+    del blip_model
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # load model
+    model = load_model(config_file, grounded_checkpoint, device=device)
     # run grounding dino model
     boxes_filt, scores, pred_phrases = get_grounding_output(
         model, image, text_prompt, box_threshold, text_threshold, device=device
@@ -286,15 +328,17 @@ if __name__ == "__main__":
     caption = check_caption(caption, pred_phrases)
     print(f"Revise caption with number: {caption}")
 
-    transformed_boxes = predictor.transform.apply_boxes_torch(boxes_filt, image.shape[:2]).to(device)
+    transformed_boxes = predictor.transform.apply_boxes_torch(
+        boxes_filt, image.shape[:2]
+    ).to(device)
 
     masks, _, _ = predictor.predict_torch(
-        point_coords = None,
-        point_labels = None,
-        boxes = transformed_boxes.to(device),
-        multimask_output = False,
+        point_coords=None,
+        point_labels=None,
+        boxes=transformed_boxes.to(device),
+        multimask_output=False,
     )
-    
+
     # draw output image
     plt.figure(figsize=(10, 10))
     plt.imshow(image)
@@ -304,10 +348,12 @@ if __name__ == "__main__":
         show_box(box.numpy(), plt.gca(), label)
 
     plt.title(caption)
-    plt.axis('off')
+    plt.axis("off")
     plt.savefig(
-        os.path.join(output_dir, "automatic_label_output.jpg"), 
-        bbox_inches="tight", dpi=300, pad_inches=0.0
+        os.path.join(output_dir, "automatic_label_output.jpg"),
+        bbox_inches="tight",
+        dpi=300,
+        pad_inches=0.0,
     )
 
     save_mask_data(output_dir, caption, masks, boxes_filt, pred_phrases)
