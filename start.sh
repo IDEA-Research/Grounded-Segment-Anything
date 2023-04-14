0b2e256cb954d11039cf45b56a5dd42ddea3d570
Julienlin <julien.lin@osedea.com>
2023-04-14 07:16:32 -0400

    remove ssl

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn --workers=1 -t 300 --certfile=server.crt --keyfile=server.key --bind="0.0.0.0:9999" backend_gpu.app:app
+gunicorn --workers=1 -t 300 --bind="0.0.0.0:9999" backend_gpu.app:app
