7ab490018a8f65cb85e3d27130236979229fa974
yukang <yukangchen@cse.cuhk.edu.hk>
2023-04-19 00:21:17 +0800

    Add files via upload

---
+++ voxelnext_3d_box/models/mean_vfe.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+class MeanVFE(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, batch_dict, **kwargs):
+        """
+        Args:
+            batch_dict:
+                voxels: (num_voxels, max_points_per_voxel, C)
+                voxel_num_points: optional (num_voxels)
+            **kwargs:
+
+        Returns:
+            vfe_features: (num_voxels, C)
+        """
+        voxel_features, voxel_num_points = batch_dict['voxels'], batch_dict['voxel_num_points']
+        points_mean = voxel_features[:, :, :].sum(dim=1, keepdim=False)
+        normalizer = torch.clamp_min(voxel_num_points.view(-1, 1), min=1.0).type_as(voxel_features)
+        points_mean = points_mean / normalizer
+        batch_dict['voxel_features'] = points_mean.contiguous()
+
+        return batch_dict
---
+++ voxelnext_3d_box/utils/config.py
@@ -0,0 +1,34 @@
+import yaml
+from easydict import EasyDict
+
+def merge_new_config(config, new_config):
+    if '_BASE_CONFIG_' in new_config:
+        with open(new_config['_BASE_CONFIG_'], 'r') as f:
+            try:
+                yaml_config = yaml.safe_load(f, Loader=yaml.FullLoader)
+            except:
+                yaml_config = yaml.safe_load(f)
+        config.update(EasyDict(yaml_config))
+
+    for key, val in new_config.items():
+        if not isinstance(val, dict):
+            config[key] = val
+            continue
+        if key not in config:
+            config[key] = EasyDict()
+        merge_new_config(config[key], val)
+
+    return config
+
+def cfg_from_yaml_file(cfg_file, config):
+    with open(cfg_file, 'r') as f:
+        try:
+            new_config = yaml.safe_load(f, Loader=yaml.FullLoader)
+        except:
+            new_config = yaml.safe_load(f)
+
+        merge_new_config(config=config, new_config=new_config)
+
+    return config
+
+cfg = EasyDict()
